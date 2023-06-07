7739bcae5afb92fd032eb906204810546e08558c
pipixin321 <1967317342@qq.com>
2023-06-07 21:08:36 +0800

    modified:   backbone/model_cfgs.py

--- backbone/model_cfgs.py
+++ backbone/model_cfgs.py
@@ -1,45 +1,10 @@
-
+import os
+ckpt_path='/mnt/data1/zhx/TAL_APP/backbone/ckpt'
 MODEL_CFGS={
     'xxx':{
         'data_pipeline':[],
-        'model_cfg':dict()
-    },
-    'i3d':{
-        'data_pipeline':
-            [
-            dict(type='UntrimmedSampleFrames',clip_len=32,frame_interval=8,start_index=1),
-            dict(type='RawFrameDecode'),
-            dict(type='Resize', scale=(-1, 256)),
-            dict(type='CenterCrop', crop_size=256),
-            dict(type='Normalize',mean=[123.675, 116.28, 103.53],std=[58.395, 57.12, 57.375],to_bgr=False),
-            dict(type='FormatShape', input_format='NCTHW'),
-            dict(type='Collect', keys=['imgs'], meta_keys=[]),
-            dict(type='ToTensor', keys=['imgs'])
-            ],
-        'model_cfg':
-            dict(
-            type='Recognizer3D',
-            backbone=dict(
-                type='ResNet3d',
-                pretrained2d=False,
-                pretrained='torchvision://resnet50',
-                depth=50,
-                conv1_kernel=(5, 7, 7),
-                conv1_stride_t=2,
-                pool1_stride_t=2,
-                conv_cfg=dict(type='Conv3d'),
-                norm_eval=False,
-                inflate=((1, 1, 1), (1, 0, 1, 0), (1, 0, 1, 0, 1, 0), (0, 1, 0)),
-                zero_init_residual=False),
-            cls_head=dict(
-                type='I3DHead',
-                num_classes=400,
-                in_channels=2048,
-                spatial_type='avg',
-                dropout_ratio=0.5,
-                init_std=0.01),
-            test_cfg=dict(average_clips=None,feature_extraction=True)),
-        'ckpt':'/mnt/data1/zhx/TAL_APP/backbone/ckpt/i3d_r50_32x2x1_100e_kinetics400_rgb_20200614-c25ef9a4.pth'
+        'model_cfg':dict(),
+        'ckpt':''
     },
 
     'swin_tiny':{
@@ -77,6 +42,96 @@ MODEL_CFGS={
                 num_classes=400,
                 spatial_type='avg'),
             test_cfg=dict(average_clips=None,feature_extraction=True)),
-        'ckpt':'/mnt/data1/zhx/TAL_APP/backbone/ckpt/swin_tiny_patch244_window877_kinetics400_1k.pth'
-    }
+        'ckpt':os.path.join(ckpt_path,'swin_tiny_patch244_window877_kinetics400_1k.pth')
+    },
+
+    'i3d':{
+        'data_pipeline':
+            [
+            dict(type='UntrimmedSampleFrames',clip_len=32,frame_interval=8,start_index=1),
+            dict(type='RawFrameDecode'),
+            dict(type='Resize', scale=(-1, 256)),
+            dict(type='CenterCrop', crop_size=256),
+            dict(type='Normalize',mean=[123.675, 116.28, 103.53],std=[58.395, 57.12, 57.375],to_bgr=False),
+            dict(type='FormatShape', input_format='NCTHW'),
+            dict(type='Collect', keys=['imgs'], meta_keys=[]),
+            dict(type='ToTensor', keys=['imgs'])
+            ],
+        'model_cfg':
+            dict(
+            type='Recognizer3D',
+            backbone=dict(
+                type='ResNet3d',
+                pretrained2d=True,
+                pretrained='torchvision://resnet50',
+                depth=50,
+                conv1_kernel=(5, 7, 7),
+                conv1_stride_t=2,
+                pool1_stride_t=2,
+                conv_cfg=dict(type='Conv3d'),
+                norm_eval=False,
+                inflate=((1, 1, 1), (1, 0, 1, 0), (1, 0, 1, 0, 1, 0), (0, 1, 0)),
+                zero_init_residual=False),
+            cls_head=dict(
+                type='I3DHead',
+                num_classes=400,
+                in_channels=2048,
+                spatial_type='avg',
+                dropout_ratio=0.5,
+                init_std=0.01),
+            test_cfg=dict(average_clips=None,feature_extraction=True)),
+        'ckpt':os.path.join(ckpt_path,'i3d_r50_32x2x1_100e_kinetics400_rgb_20200614-c25ef9a4.pth')
+    },
+    
+    'slowfast101':{
+        'data_pipeline':
+            [
+            dict(type='UntrimmedSampleFrames',clip_len=32,frame_interval=8,start_index=1),
+            dict(type='RawFrameDecode'),
+            dict(type='Resize', scale=(-1, 256)),
+            dict(type='CenterCrop', crop_size=256),
+            dict(type='Normalize',mean=[123.675, 116.28, 103.53],std=[58.395, 57.12, 57.375],to_bgr=False),
+            dict(type='FormatShape', input_format='NCTHW'),
+            dict(type='Collect', keys=['imgs'], meta_keys=[]),
+            dict(type='ToTensor', keys=['imgs'])
+            ],
+        'model_cfg':
+            dict(
+            type='Recognizer3D',
+            backbone=dict(
+                type='ResNet3dSlowFast',
+                pretrained=None,
+                resample_rate=4,  # tau
+                speed_ratio=4,  # alpha
+                channel_ratio=8,  # beta_inv
+                slow_pathway=dict(
+                    type='resnet3d',
+                    depth=101,
+                    pretrained=None,
+                    lateral=True,
+                    fusion_kernel=7,
+                    conv1_kernel=(1, 7, 7),
+                    dilations=(1, 1, 1, 1),
+                    conv1_stride_t=1,
+                    pool1_stride_t=1,
+                    inflate=(0, 0, 1, 1),
+                    norm_eval=False),
+                fast_pathway=dict(
+                    type='resnet3d',
+                    depth=101,
+                    pretrained=None,
+                    lateral=False,
+                    base_channels=8,
+                    conv1_kernel=(5, 7, 7),
+                    conv1_stride_t=1,
+                    pool1_stride_t=1,
+                    norm_eval=False)),
+            cls_head=dict(
+                type='SlowFastHead',
+                in_channels=2304,  # 2048+256
+                num_classes=400,
+                spatial_type='avg'),
+            test_cfg=dict(average_clips=None,feature_extraction=True)),
+        'ckpt':os.path.join(ckpt_path,'slowfast_r101_8x8x1_256e_kinetics400_rgb_20210218-0dd54025.pth')
+    },
 }
