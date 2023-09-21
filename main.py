6f2867998283a44f7c46c068696e1f9122ed6e2e
pipixin321 <1967317342@qq.com>
2023-09-21 15:52:19 +0800

    20230921

--- main.py
+++ main.py
@@ -32,15 +32,15 @@ def tal_func(video, new_short, backbone, detector, score_thresh, overlap_thresh,
     return outvid, trimmed_vid, results
 
 def train_network(filedir):
-
+    # Todo: training function
     return filedir
 
 if __name__ == "__main__":
     theme = 'soft' #options: base, Monochrome, Soft, Glass
     inputs=[gr.Video(label='输入视频 (Input Video)'),
         gr.Slider(0, 640, value=180, step=10, label="视频尺寸 (video size)", info="调整输入视频短边长度, 0为原始尺寸(resize video's short side to a new value, set 0 to keep the original size)"),
-        gr.inputs.Radio(['I3D','SlowFast','CSN','SwinViViT'], default='I3D', label='视频特征提取网络 (Video Feature Extraction Network)'),
-        gr.inputs.Radio(['ActionFormer(Fully-supervised)','CoRL(Weakly-Supervised)'], default='ActionFormer(Fully-supervised)', label='时序行为检测网络 (Temporal Action Localization Network)'),
+        gr.components.Radio(['I3D','SlowFast','CSN','SwinViViT'],  label='视频特征提取网络 (Video Feature Extraction Network)'),
+        gr.components.Radio(['ActionFormer(Fully-supervised)','CoRL(Weakly-Supervised)'], label='时序行为检测网络 (Temporal Action Localization Network)'),
         gr.Slider(0, 1, value=0.2, step=0.1, label='[后处理]置信度阈值 ([Postprocess]Score threshold)'),
         gr.Slider(0, 1, value=0.9, step=0.1, label='[后处理]重叠阈值 ([Postprocess]Overlap threshold)')]
     outputs=[gr.Video(label='输出视频 (Output Video)'),
@@ -57,18 +57,25 @@ if __name__ == "__main__":
                         outputs, 
                         examples=examples,
                         cache_examples=False,
-                        theme=theme,  
+                        theme=theme, 
                         # title='时序行为检测演示界面 (GUI Demo Of Temporal Action Localization)',
-                        description='时序行为检测是指在时序上定位未裁剪长视频中的行为片段并分类 \
-                                    \n(Temporal Action Localization attempts to temporally localize and classify action instances in the untrimmed video) \
-                                    \n输入:视频(.mp4) 输出:检测后的视频(.mp4) & 检测结果 \
-                                    \n(Input:Video(.mp4) Output:Video(.mp4) & Localization Results)')
+                        description="**时序行为检测**是指在时序上**定位**未裁剪长视频中的行为片段并**分类** \
+                                    \n(**Temporal Action Localization** attempts to temporally **localize** and **classify** action instances in the untrimmed video) \
+                                    \n- **输入**: 视频[.mp4] \
+                                    \n- **输出**: 检测后的视频[.mp4] & 裁剪后的视频 & 检测结果")
     demo2 = gr.Interface(train_network, inputs=gr.File(label='数据集文件 (Dataset File)'), outputs=gr.Text(), theme=theme,
                         description='上传规范化的数据集压缩包,训练网络')
+    
+    css = ".gradio-container { \
+    background-image: url('file=figs/HUST.jpg');\
+    background-size: 100% 100%;\
+    background-repeat: no-repeat;\
+    background-attachment: fixed;}"
     demo = gr.TabbedInterface([demo1, demo2],
                               tab_names=['Localization', 'Train Network'],
-                              title='时序行为检测演示界面 (GUI Demo Of Temporal Action Localization)',
+                              title='时序行为检测演示界面 (WebUI Demo Of Temporal Action Localization)',
                               theme=theme,
+                              css=css,
                               )
     
     # demo.launch(share=True,auth=('zhx','123'))
